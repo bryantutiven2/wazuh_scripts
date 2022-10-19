2a05999032cb75d41491a88367168b96acf843d9
bryantutiven2 <bryan.tutiven.2@outlook.com>
2022-10-19 17:10:03 -0500

    Se implementa pequeña db para realizar test

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import wazuhServer
+import json
+
+eventsType = [
+    {
+        "name": "Security",
+        "types": [
+            {
+                "name": "Login Success",
+                "case": "Brute Force",
+                "sources": [
+                    {
+                        "name": "Windows",
+                        "ids" : [60122]
+                    }
+                ]
+            }
+        ]
+    }
+]
+
 
 def main():
-    wazuhServer.conectWazuh()
+    f = open('db.json')
+    data = json.load(f)
+    for log in data['logs']:
+        user = log['_source']['data']['win']['eventdata']['targetUserName']
+        
 
 main()
